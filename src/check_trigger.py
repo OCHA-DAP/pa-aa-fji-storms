737040a7eb3c2b12ce986d34fa14bf98e69bffed
t-downing <downing.tristan@gmail.com>
2023-09-11 10:16:10 -0700

    add csv to yml

--- src/check_trigger.py
+++ src/check_trigger.py
@@ -1,4 +1,34 @@
+import msal
+
+
+def acquire_token_func():
+    """
+    Acquire token via MSAL
+    """
+    authority_url = "https://login.microsoftonline.com/unitednations"
+    app = msal.ConfidentialClientApplication(
+        authority=authority_url,
+        client_id="{client_id}",
+        client_credential="{client_secret}",
+    )
+    token = app.acquire_token_for_client(
+        scopes=["https://graph.microsoft.com/.default"]
+    )
+    return token
+
+
+def get_onedrive_file():
+    # token = acquire_token_func()
+    pass
+
+
 def check_trigger():
+    """
+    Checks trigger, from GitHub Action
+    Returns
+    -------
+
+    """
     print("checked trigger")
 
 
