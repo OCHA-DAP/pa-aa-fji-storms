085854c16061e82e23ca5f6cb6b3a8da9b09e962
t-downing <downing.tristan@gmail.com>
2023-12-12 10:13:34 +0000

    switch to vars from secrets

--- src/testrun_workflow.py
+++ src/testrun_workflow.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-YASA = os.getenv("YASA")
 OWNER = "OCHA-DAP"
 REPO = "pa-aa-fji-storms"
 WORKFLOW_ID = "check-trigger.yml"
 GH_ACTIONS_TOKEN = os.getenv("GH_ACTIONS_TOKEN")
+TEST_CSV = os.getenv("TEST_CSV")
 
 
 def testrun_workflow():
@@ -29,7 +29,7 @@ def testrun_workflow():
         "Authorization": f"Bearer {GH_ACTIONS_TOKEN}",
         "X-GitHub-Api-Version": "2022-11-28",
     }
-    body = {"ref": "add-trigger", "inputs": {"csv": YASA}}
+    body = {"ref": "add-trigger", "inputs": {"csv": TEST_CSV}}
     response = requests.post(url=url, headers=headers, data=json.dumps(body))
     print(response.status_code)
 
