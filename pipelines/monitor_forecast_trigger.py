302ab31b1727a220846dfe7efc184b9f24dd0491
Tristan Downing <downing.tristan@gmail.com>
2025-12-11 13:03:43 -0800

    print env vars

--- pipelines/monitor_forecast_trigger.py
+++ pipelines/monitor_forecast_trigger.py
@@ -1,10 +1,17 @@
+import os
+
 from src.listmonk import create_campaign, send_campaign
 from src.logger import get_logger
 
 logger = get_logger(__name__)
 
+TEST_EMAIL = os.getenv("TEST_EMAIL", "True")
+FORCE_TRIGGER = os.getenv("FORCE_TRIGGER", "False")
+
 if __name__ == "__main__":
     logger.info("Monitor forecast trigger pipeline started.")
+    logger.info(f"TEST_EMAIL: {TEST_EMAIL}")
+    logger.info(f"FORCE_TRIGGER: {FORCE_TRIGGER}")
     campaign_id = create_campaign()
     logger.info(f"Created campaign with ID: {campaign_id}")
     send_campaign(campaign_id)
